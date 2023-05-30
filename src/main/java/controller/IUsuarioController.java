94522950216c027e7889373bacb0e3eed0438640
LuisAlfonso <luisalfonsomartinez@live.com>
2023-05-29 19:53:56 -0500

    segundo commit

--- src/main/java/controller/IUsuarioController.java
+++ src/main/java/controller/IUsuarioController.java
@@ -1,9 +1,13 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package controller;
- import java.util.Map;
 
+/**
+ *
+ * @author Luis Alfonso Martine
+ */
 public class IUsuarioController {
     
-     
-      
 }
