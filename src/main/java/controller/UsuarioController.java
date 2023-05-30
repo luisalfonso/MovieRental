728fa7383c6d12d05fb9346854999ed1b47a463e
LuisAlfonso <luisalfonsomartinez@live.com>
2023-05-29 19:54:06 -0500

    segundo commit

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -1,8 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package controller;
 
-import java.sql.ResultSet;
-import java.sql.Statement;
-import com.google.gson.Gson;
-
-import beans.Usuario;
-import connection.DBConnection;
+/**
+ *
+ * @author Luis Alfonso Martine
+ */
+public class UsuarioController {
+    
+}
